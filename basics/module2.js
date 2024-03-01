c8f0fe37ab2c7f3a687813c4eb4b06c3fb8137e0
SahajArora <sahajarora1906@gmail.com>
2024-03-01 10:34:16 +0530

    always export your functions in the end using module.exports

---
+++ basics/module2.js
@@ -0,0 +1,12 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//(with destructuring)
+// const { add, sub } = require("./module1.js");
+// console.log(add(2, 4));
+// console.log(sub(4, 2));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//(without destructuring)
+// const math = require("./module1.js");
+// console.log(math);
+// console.log(math.add(2, 4));
+// console.log(math.sub(4, 2));
